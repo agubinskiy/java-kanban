3e19542f4667ca8d2fa2de6b1fa03573eabf585f
GAV <alexanderg55@yandex.ru>
2025-04-08 21:09:58 +0300

    Скорректирован InMemoryHistoryManager, класс Node перемещён внутрь

--- src/history/InMemoryHistoryManager.java
+++ src/history/InMemoryHistoryManager.java
@@ -23,7 +23,7 @@ public class InMemoryHistoryManager implements HistoryManager {
         linkLast(task);
         if (tail == null)
             tasksLinkedMap.put(taskId, head);
-         else
+        else
             tasksLinkedMap.put(taskId, tail);
     }
 
@@ -93,54 +93,52 @@ public class InMemoryHistoryManager implements HistoryManager {
             nextNode.setPrev(prevNode);
         }
     }
-}
 
- class Node {
-    private Task data;
-    private Node prev;
-    private Node next;
+    static class Node {
+        private Task data;
+        private Node prev;
+        private Node next;
 
-    public Node(Task data) {
-        this.data = data;
-        this.prev = null;
-        this.next = null;
+        public Node(Task data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+
+        public Task getData() {
+            return data;
+        }
+
+        public void setData(Task data) {
+            this.data = data;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node = (Node) o;
+            return Objects.equals(data, node.data) && Objects.equals(prev, node.prev) && Objects.equals(next, node.next);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(data, prev, next);
+        }
     }
-
-
-    public Task getData() {
-        return data;
-    }
-
-    public void setData(Task data) {
-        this.data = data;
-    }
-
-    public Node getPrev() {
-        return prev;
-    }
-
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Node node = (Node) o;
-        return Objects.equals(data, node.data) && Objects.equals(prev, node.prev) && Objects.equals(next, node.next);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(data, prev, next);
-    }
-
 }
