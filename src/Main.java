4f770d8f52310eea86326eb102821b3ef34443a3
GAV <alexanderg55@yandex.ru>
2025-04-18 15:24:44 +0300

    Исправлены замечания по codestyle

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) throws IOException {
         TaskManager taskManager = Managers.getDefault();
-        taskManager = FileBackedTaskManager.loadFromFile(Paths.get(System.getProperty("user.home"),
-                "testFile.txt").toFile());
 
 
         Task task1 = new Task("Задача1", "Описание задачи1");
--- src/Managers.java
+++ src/Managers.java
@@ -2,9 +2,6 @@ import history.HistoryManager;
 import history.InMemoryHistoryManager;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class Managers {
     public static TaskManager getDefault() {
