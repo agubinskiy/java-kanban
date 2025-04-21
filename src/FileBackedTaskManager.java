e5c464bec229456b262b364f7ec12cbed736fbcb
GAV <alexanderg55@yandex.ru>
2025-04-21 17:00:11 +0300

    Удалён неиспользующийся импорт

--- src/FileBackedTaskManager.java
+++ src/FileBackedTaskManager.java
@@ -1,6 +1,9 @@
 import tasks.*;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.util.List;
 
