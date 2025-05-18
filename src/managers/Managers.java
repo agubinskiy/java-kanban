034709103b7d611d2807e60805cab5de3ba78558
GAV <alexanderg55@yandex.ru>
2025-05-18 14:58:08 +0300

    Удалён неиспользуемый импорт

--- src/managers/Managers.java
+++ src/managers/Managers.java
@@ -3,8 +3,6 @@ package managers;
 import history.HistoryManager;
 import history.InMemoryHistoryManager;
 
-import java.io.File;
-
 public class Managers {
     public static TaskManager getDefault() {
         //return new FileBackedTaskManager(new File(System.getProperty("user.home"), "testFile.txt"));
